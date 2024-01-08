9f0acfadfa0b1884643272ab67a53c7cabfb8f57
juber <juberk1010@gmail.com>
2024-01-08 23:22:21 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import './App.css';
 
 function App() { 
   const [image , setImage] = useState("");
-  const [input , setInput] = useState("dog");
+  const [input , setInput] = useState("");
   const [loading , setLoading] = useState(false);
+  const [error , setError] = useState(false);
+  const [errorMessage , setErrorMessage] = useState("");
 
  const fetchApi = async() => {
   try{
     setLoading(true)
     const res = await fetch(
-      "https://api-inference.huggingface.co/models/prompthero/openjourney",
+      "https://api-inference.huggingface.co/models/prompthero/openjourney-v4",
       {
       method: "POST",
       headers: {
@@ -22,10 +24,12 @@ function App() {
     )
     const blob =  await res.blob();
     setImage(URL.createObjectURL(blob))
-    setLoading(false)
+    setLoading(false);
+    setError(false);
     console.log(image);
   }catch(err){
-    console.log(err)
+    setError(true);
+    setErrorMessage(err);
   }
 
 }
@@ -35,10 +39,10 @@ function App() {
      fetchApi();
   }
 
-useEffect(() => {
-  fetchApi();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-},[input])
+// useEffect(() => {
+//   fetchApi();
+//   // eslint-disable-next-line react-hooks/exhaustive-deps
+// },[input])
   
 
 
@@ -47,13 +51,14 @@ useEffect(() => {
       <div className="search-bar flex items-center justify-center ">
         <form onSubmit={handleSubmit} className='flex w-full'>
           <input type="text" placeholder='search...' className=' rounded-md p-2  border focus:outline-none text-xl' onChange={(e) => setInput(e.target.value)}/>
-          <button type='submit' className='p-2 border rounded-md bg-black text-white'>Search!</button>
+          <button type='submit' className='p-2 border rounded-md bg-black text-white'>Generate!</button>
         </form>
       </div>
 
       <div className="card m-3">
         <div className="card flex items-center justify-center">
           {loading ? "Loading...." : <img src={image} alt=""  className='rounded shadow-xl' width={400} height={400}/>}
+          {error && errorMessage}
         </div>
       </div>
     </div>
