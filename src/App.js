4a17b0183d211d933ea7c4ef010a1d7ca64ecdf1
juber <juberk1010@gmail.com>
2024-01-08 23:08:12 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
+function App() { 
+  const [image , setImage] = useState("");
+  const [input , setInput] = useState("dog");
+  const [loading , setLoading] = useState(false);
+
+ const fetchApi = async() => {
+  try{
+    setLoading(true)
+    const res = await fetch(
+      "https://api-inference.huggingface.co/models/prompthero/openjourney",
+      {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        "Authorization": "Bearer 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"
+      },
+      body: JSON.stringify({ inputs: input }),
+    }
+    )
+    const blob =  await res.blob();
+    setImage(URL.createObjectURL(blob))
+    setLoading(false)
+    console.log(image);
+  }catch(err){
+    console.log(err)
+  }
+
+}
+
+  const handleSubmit  = async(e) => {
+     e.preventDefault();
+     fetchApi();
+  }
+
+useEffect(() => {
+  fetchApi();
+},[input])
+  
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App w-4/5 m-auto p-3 flex items-center flex-col justify-between gap-3">
+      <div className="search-bar flex items-center justify-center ">
+        <form onSubmit={handleSubmit} className='flex w-full'>
+          <input type="text" placeholder='search...' className=' rounded-md p-2  border focus:outline-none text-xl' onChange={(e) => setInput(e.target.value)}/>
+          <button type='submit' className='p-2 border rounded-md bg-black text-white'>Search!</button>
+        </form>
+      </div>
+
+      <div className="card m-3">
+        <div className="card flex items-center justify-center">
+          {loading ? "Loading...." : <img src={image} alt=""  className='rounded shadow-xl' width={400} height={400}/>}
+        </div>
+      </div>
     </div>
   );
 }
